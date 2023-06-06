ee585c05a1764c8ba3c8c0f322955be1a060099d
Patrick Coutinho <patrick@istyle.com.br>
2023-06-06 17:16:51 -0300

    backend sample

--- backend/src/resolvers/user.ts
+++ backend/src/resolvers/user.ts
@@ -1,9 +1,25 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "../models/User";
+import { randomUUID } from "node:crypto";
 
 @Resolver()
 export class UserResolver {
-  @Query(() => String)
-  async hello() {
-    return "Hello";
+  private data: User[] = [];
+
+  @Query(() => [User])
+  async users() {
+    return this.data;
+  }
+
+  @Mutation(() => User)
+  async createUser(@Arg("name") name: string) {
+    const user: User = {
+      id: randomUUID().toString(),
+      name,
+    };
+
+    this.data.push(user);
+
+    return user;
   }
 }
