98e842a52cd79885d4685078a0f1232192217a63
Patrick Coutinho <patrick@istyle.com.br>
2023-06-07 17:24:23 -0300

    frontend sample

---
+++ frontend/src/components/NewUserForm.tsx
@@ -0,0 +1,47 @@
+import { gql, useMutation } from "@apollo/client";
+import { FormEvent, useState } from "react";
+import { GET_USER } from "../App";
+
+const CREATE_USER = gql`
+  mutation ($name: String!) {
+    createUser(name: $name) {
+      id
+      name
+    }
+  }
+`;
+
+export function NewUserForm() {
+  const [name, setName] = useState("");
+  const [createUser, { loading }] = useMutation(CREATE_USER);
+
+  async function handleCreateUser(event: FormEvent) {
+    event.preventDefault();
+
+    if (!name) {
+      return;
+    }
+
+    await createUser({
+      variables: {
+        name,
+      },
+      refetchQueries: [GET_USER],
+    });
+  }
+
+  if (loading) {
+    return <h1>Carregando...</h1>;
+  }
+
+  return (
+    <form onSubmit={handleCreateUser}>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button type="submit">Enviar</button>
+    </form>
+  );
+}
